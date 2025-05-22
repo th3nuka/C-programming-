dfe969f2c764b4074bea0c048d4ccbf10a589f7e
th3nuka <thenukamunasinghe00@gmail.com>
2025-05-22 15:58:30 +0530

    Add files via upload

---
+++ Factorial of a number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n, fact = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    printf("Factorial of %d = %d\n", n, fact);
+    return 0;
+}
